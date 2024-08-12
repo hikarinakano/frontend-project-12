3281854c2519b7d37b2f50a833db658dd94993f9
Irina <hikarinakano95@gmail.com>
2024-08-12 16:57:09 +0200

    Created new pages for 404 error and for login form

---
+++ frontend/src/Components/NotFound.js
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom';
+
+export const NotFound = () => (
+  <nav>
+    <h1>404 Error.Page was not found</h1>
+    <ul>
+      <li>
+        <Link to="/">Go back to Main page</Link>
+      </li>
+      <li>
+        <Link to="/login">Go back to LogIn page</Link>
+      </li>
+    </ul>
+  </nav>
+);
