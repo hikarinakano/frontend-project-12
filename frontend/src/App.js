bbd7b1ea41649b61abd27be40a2728892253a339
Irina <hikarinakano95@gmail.com>
2024-09-03 15:35:06 +0200

    Recreate the Components needed in reference app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,76 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PageOne } from './Components/Pages';
-import { NotFound } from './Components/NotFound';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
+import { Button, Navbar, Nav } from 'react-bootstrap';
 import LoginPage  from './Components/Form';
-const App = () => {
+import { NotFound } from './Components/NotFound.js';
+import PrivatePage from './Components/PrivateChat';
+import AuthContext from './contexts/index.jsx';
+import useAuth from './hooks/index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PageOne />} />
-        <Route path="/login" element={< LoginPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
   );
+};
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
+const AuthButton = () => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn
+      ? <Button onClick={auth.logOut}>Log out</Button>
+      : <Button as={Link} to="/login" state={{ from: location }}>Log in</Button>
+  );
+};
+
+const App = () => {
+  return (<AuthProvider>
+  <Router>
+    <div className="container p-3">
+      <h1 className="text-center mt-5 mb-4">Welcome to the secret chat!</h1>
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/error" element={<NotFound />} />
+        {/* <Route
+          path="/private"
+          element={(
+            <PrivateRoute>
+              <PrivatePage />
+            </PrivateRoute>
+          )}
+        /> */}
+      </Routes>
+    </div>
+
+  </Router>
+</AuthProvider>)
 }
 
 export default App;
