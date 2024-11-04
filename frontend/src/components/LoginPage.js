21e848e84b8c454aa2bc341a9786e913b72fa877
Irina <hikarinakano95@gmail.com>
2024-11-04 14:35:01 +0100

    Removed the React from the useState import

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useFormik } from 'formik';
 import { Button, Form } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
