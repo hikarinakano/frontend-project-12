a4748a0b605f2f31a75bba6c1fb022957aab4ecf
Irina <hikarinakano95@gmail.com>
2024-11-05 11:28:56 +0100

    Renamed the usersSlice to authSlice

---
+++ frontend/src/store/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  username: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuthInfo: (state, action) => {
+      state.username = action.payload.username;
+      state.token = action.payload.token;
+    },
+    clearAuthInfo: (state) => {
+      state.username = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setAuthInfo, clearAuthInfo } = authSlice.actions;
+export default authSlice.reducer;
