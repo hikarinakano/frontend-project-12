53e491fc1176acefa1d382940c2bca8e5147c6e5
Irina <hikarinakano95@gmail.com>
2024-11-05 14:47:35 +0100

    Added new Query reducer and concatenated the new query middleware

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../store/slices/authSlice';
 import { channelsApi } from "./api/channelsApi";
+import { messagesApi } from "./api/messagesApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(channelsApi.middleware),
+    getDefaultMiddleware()
+    .concat(channelsApi.middleware)
+    .concat(messagesApi.middleware),
 });
 
 
