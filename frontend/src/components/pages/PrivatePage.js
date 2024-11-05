34af00db35112f2c79ba0a7e8e69aa30c7bdd2b6
Irina <hikarinakano95@gmail.com>
2024-11-05 14:22:04 +0100

    Rewrote the future chat component to display channels fetched from the server

--- frontend/src/components/pages/PrivatePage.js
+++ frontend/src/components/pages/PrivatePage.js
@@ -1,32 +1,19 @@
-// import React from "react";
-// import axios from 'axios';
-// // import routes from '../routes.js';
-// // const getAuthHeader = () => {
-// //   const userId = JSON.parse(localStorage.getItem('userId') ?? '{}');
-// //   if (userId && userId.token) {
-// //     return { Authorization: `Bearer ${userId.token}` };
-// //   }
+import { useGetChannelsQuery } from '../../store/api/channelsApi';
 
-// //   return {};
-// // };
+const PrivatePage = () => {
+  const { data: channels, isLoading, error } = useGetChannelsQuery();
 
-const PrivatePage =  () => {
-  // const fetchContent = async () => {
-    // const channels = await axios.get(routes.channelsPath(), {headers: getAuthHeader()});
-    // const messages = await axios.post(routes.messagesPath(), {message: 'hello'}, {headers: getAuthHeader()});
-    // the question of connecting channels and messages is still open how. The structure of the slices and the server responses will be different
-  // }
-  // fetchContent();
-  // useEffect(() => {
-  //   const fetchContent = async () => {
-  //     console.log(getAuthHeader())
-  //     await axios.post(routes.loginPath(), { headers: getAuthHeader() });
-  //   };
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
-  //   fetchContent();
-  // }, []);
-  return <div>Congrats you are logged in!</div>
-  // END
+  return (
+    <div>
+      <h2>Channels</h2>
+      {channels?.map((channel) => (
+        <div key={channel.id}>{channel.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PrivatePage;
