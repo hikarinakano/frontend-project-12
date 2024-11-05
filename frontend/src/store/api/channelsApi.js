532e2fa10aab99a34c7d1ff456059a3cafceedff
Irina <hikarinakano95@gmail.com>
2024-11-05 14:20:57 +0100

    Created an RTK Query for fetching existing channels of a specific user

--- frontend/src/store/api/channelsApi.js
+++ frontend/src/store/api/channelsApi.js
@@ -1,1 +1,32 @@
-// i need to fetch all channels and render for the user
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import routes from '../../routes';
+
+export const channelsApi = createApi({
+  reducerPath: 'channels',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/',
+    prepareHeaders: (headers, { getState }) => {
+      // Try getting token from Redux state first
+      const { token } = getState().auth;
+      
+      if (!token) {
+        // If no token in Redux, try localStorage
+        const stored = localStorage.getItem('userId');
+        if (stored) {
+          const parsedAuth = JSON.parse(stored);
+          headers.set('Authorization', `Bearer ${parsedAuth.token}`);
+        }
+      } else {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getChannels: builder.query({
+      query: () => routes.channelsPath(),
+    }),
+  }),
+});
+
+export const { useGetChannelsQuery } = channelsApi;
