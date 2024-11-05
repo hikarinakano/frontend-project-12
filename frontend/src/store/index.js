3c7b82ec28f76ef1bca1bed01ae17a9da5d6405a
Irina <hikarinakano95@gmail.com>
2024-11-05 14:21:23 +0100

    Configured new query in the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from '../store/slices/authSlice'
+import authReducer from '../store/slices/authSlice';
+import { channelsApi } from "./api/channelsApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [channelsApi.reducerPath]: channelsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(channelsApi.middleware),
 });
+
 
 export default store;
