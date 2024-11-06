a89fd63c986c03516abbcbfb891d958772a50141
Irina <hikarinakano95@gmail.com>
2024-11-06 17:22:47 +0100

    Added a div for adding new Channels

--- frontend/src/components/pages/PrivatePage.js
+++ frontend/src/components/pages/PrivatePage.js
@@ -17,18 +17,17 @@ const PrivatePage = () => {
   if (isLoading || !selectedChannel) {
     return <div>Loading...</div>;
   }
-
   return (
     <div className="container-fluid h-100 p-0">
       <div className="row h-100 g-0">
         <div className="col-3 col-md-2 border-end pt-5 bg-light">
+          <div className=" mb-2 d-flex flex-column nav nav-pills nav-fill px-2">+ Add channel</div>
           <div className="d-flex flex-column nav nav-pills nav-fill px-2">
             {channels?.map((channel) => (
               <button
                 key={channel.id}
-                className={`nav-link text-start mb-2 ${
-                  selectedChannel === channel.id ? 'active' : ''
-                }`}
+                className={`nav-link text-start mb-2 ${selectedChannel === channel.id ? 'active' : ''
+                  }`}
                 onClick={() => setSelectedChannel(channel.id)}
               >
                 # {channel.name}
