3677ee374fc6d5bdde57b826505e78290e8b6d56
Irina <hikarinakano95@gmail.com>
2024-11-06 14:16:28 +0100

    Added a new route for the signup action

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
+  signupPath: () => [apiPath, 'signup'].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
 };
