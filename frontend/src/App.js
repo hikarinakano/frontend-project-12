00ba1de1e93dc128596123686beed8165f18f2be
Irina <hikarinakano95@gmail.com>
2024-11-07 11:38:34 +0100

    Removed the unnecessary /n at the top of the module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 import LoginPage from './components/pages/LoginPage.js';
