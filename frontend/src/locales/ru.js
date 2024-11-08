818770e54066ada6b5162bdfc620db39d6d8a632
Irina <hikarinakano95@gmail.com>
2024-11-08 14:54:08 +0100

    Added ru locale and initialized i18n instance in locales/index.js

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,57 @@
+export default {
+  translation: {
+    login: {
+      header: 'Войти',
+      username: 'Ваш ник',
+      password: 'Пароль',
+      submit: 'Войти',
+      noAccount: 'Нет аккаунта?',
+      signup: 'Регистрация'
+    },
+    chat: {
+      channels: 'Каналы',
+      messages_zero: '{{count}} сообщений',
+      messages_one: '{{count}} сообщение',
+      messages_few: '{{count}} сообщения',
+      messages_many: '{{count}} сообщений',
+      messageInput: 'Введите сообщение...',
+      send: 'Отправить'
+    },
+    notFound: {
+      header: 'Страница не найдена',
+      goto: 'Но вы можете перейти на ',
+      mainPage: 'главную страницу',
+    },
+    modals: {
+      add: {
+        title: 'Добавить канал',
+        submit: 'Добавить',
+        cancel: 'Отменить'
+      },
+      edit: {
+        title: 'Переименовать канал',
+        submit: 'Сохранить',
+        cancel: 'Отменить'
+      },
+      delete: {
+        title: 'Удалить канал',
+        confirm: 'Уверены?',
+        submit: 'Удалить',
+        cancel: 'Отменить'
+      }
+    },
+    errors: {
+      required: 'Обязательное поле',
+      login: 'Неверные имя пользователя или пароль',
+      unique: 'Должно быть уникальным',
+      length: 'От 3 до 20 символов',
+    },
+    warning: {
+      channelCreated: 'Канал создан',
+      connection: 'Ошибка соединения',
+      channelDeleted: 'Канал удален',
+      channelRenamed: 'Канал переименован',
+    },
+    logOutBtn: "Выйти",
+  }
+};
