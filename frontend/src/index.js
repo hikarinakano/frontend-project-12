c7bc8f33fa059b2cdcb478079138b2d21f827615
Irina <hikarinakano95@gmail.com>
2024-11-13 15:24:51 +0100

    Added and initialized the bad words filter in the project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './locales/index.js';
+import setupProfanityFilter from './services/profanityFilter.js';
+
+setupProfanityFilter();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
