1c2635f47c507f187c4b52711a0e97964770c433
Irina <hikarinakano95@gmail.com>
2024-11-14 11:57:20 +0100

    Imported the rollbar into the project. Deployed and checked for errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,48 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './locales/index.js';
 import setupProfanityFilter from './services/profanityFilter.js';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: 'f7030b6c09544a6baa9243112f15213e',
+  environment: 'testenv',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: true,
+  payload: {
+    client: {
+      javascript: {
+        source_map_enabled: true,
+        code_version: '1.0.0',
+        guess_uncaught_frames: true
+      }
+    }
+  }
+};
+
+function TestError() {
+  React.useEffect(() => {
+    const a = null;
+    try {
+      a.hello();
+    } catch (error) {
+      throw new Error('Test error for Rollbar');
+    }
+  }, []);
+
+  return null;
+}
 
 setupProfanityFilter();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider config={rollbarConfig}>
+    <React.StrictMode>
+      <ErrorBoundary>
+        <TestError />
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  </Provider>
 );
