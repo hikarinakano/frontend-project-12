b797c8477dd551448a10bd29952a9fe50b327ec8
Irina <hikarinakano95@gmail.com>
2024-11-18 17:28:34 +0100

    Linter fix

--- frontend/src/components/pages/NotFoundPage.js
+++ frontend/src/components/pages/NotFoundPage.js
@@ -3,10 +3,16 @@ import { useTranslation } from 'react-i18next';
 
 const NotFoundPage = () => {
   const { t } = useTranslation();
-  return (<nav>
-    <h1>{t('notFound.header')}</h1>
-        <div>{t('notFound.goto')}<Link to="/">{t('notFound.mainPage')}</Link></div>
-  </nav>)
+  return (
+    <nav>
+      <h1>{t('notFound.header')}</h1>
+      <div>{t('notFound.goto')}
+        <Link to="/">
+          {t('notFound.mainPage')}
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NotFoundPage;
