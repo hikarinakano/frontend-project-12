398121cd6ded58de492e9dac137d0808e5bdf020
Irina <hikarinakano95@gmail.com>
2024-11-18 16:24:53 +0100

    Linter fixes for imports order and tabulation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,8 @@
+import './styles/main.css';
+import { Provider } from 'react-redux';
+import store from '../src/store/index.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 import LoginPage from './components/pages/LoginPage.js';
@@ -5,56 +10,51 @@ import ChatPage from './components/chat/ChatPage.js';
 import SignupPage from './components/pages/SignupPage.js';
 import PrivateRoute from './components/authorization/PrivateRoute.js';
 import AuthProvider from './components/authorization/AuthProvider.js';
-import NotFoundPage from './components/pages/NotFoundPage.js'
+import NotFoundPage from './components/pages/NotFoundPage.js';
 import LogOutButton from './components/authorization/AuthButton.js';
-import store from '../src/store/index.js';
-import { Provider } from 'react-redux';
-import './styles/main.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => (
   <div className='d-flex flex-column vh-100'>
-  <Provider store={store}>
-    <AuthProvider>
-      <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <Container>
-          <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+    <Provider store={store}>
+      <AuthProvider>
+        <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <Container>
+            <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
             <LogOutButton />
-        </Container>
-      </Navbar>
-      <Router>
-        <div className="container h-100 my-4 overflow-hidden rounded shadow flex-grow-1">
-          <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route
-              path="/"
-              element={(
-                <PrivateRoute>
-                  <ChatPage />
-                </PrivateRoute>
-              )}
-            />
-            <Route path="/signup" element={<LoginPage />} />
-          </Routes>
-        </div>
-      </Router>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </AuthProvider>
-  </Provider>
+          </Container>
+        </Navbar>
+        <Router>
+          <div className="container h-100 my-4 overflow-hidden rounded shadow flex-grow-1">
+            <Routes>
+              <Route path="*" element={<NotFoundPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route
+                path="/"
+                element={(
+                  <PrivateRoute>
+                    <ChatPage />
+                  </PrivateRoute>
+                )}
+              />
+              <Route path="/signup" element={<LoginPage />} />
+            </Routes>
+          </div>
+        </Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </AuthProvider>
+    </Provider>
   </div>
 );
 
