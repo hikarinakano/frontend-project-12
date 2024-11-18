78f474c486889506accdd31757bbb0d957763ab3
Irina <hikarinakano95@gmail.com>
2024-11-18 15:42:44 +0100

    Using socket io to react for channels manipulation for all users

--- frontend/src/components/chat/ChatPage.js
+++ frontend/src/components/chat/ChatPage.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useGetChannelsQuery } from '../../store/api/channelsApi';
+import { useSelector } from 'react-redux';
 import Channels from './Channels';
 import Chat from './Chat';
+import socket from '../../services/socket';
 
 const ChatPage = () => {
   const { data: channels, isLoading } = useGetChannelsQuery();
   const [currentChannel, setCurrentChannel] = useState(null);
   const [isFirstLoad, setFirstLoad] = useState(true);
+  const { username } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (channels && isFirstLoad) {
@@ -16,6 +19,22 @@ const ChatPage = () => {
     }
   }, [channels, isFirstLoad]);
 
+  useEffect(() => {
+    socket.on('newChannel', (channel) => {
+      if (channel.username === username) {
+        setCurrentChannel(channel);
+      }
+    });
+
+    return () => {
+      socket.off('newChannel');
+    };
+  }, [username]);
+
+  const handleChannelSelect = (channel) => {
+    setCurrentChannel(channel);
+  };
+
   if (isLoading || !currentChannel) return <div>Loading...</div>;
 
   return (
@@ -24,7 +43,7 @@ const ChatPage = () => {
         <div className="col-4 col-md-2 border-end  bg-light flex-column h-100 d-flex">
           <Channels
             currentChannel={currentChannel}
-            onChannelSelect={setCurrentChannel}
+            onChannelSelect={handleChannelSelect}
           />
         </div>
         <div className="col-8 col-md-10 h-100">
