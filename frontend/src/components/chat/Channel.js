152ff4c344f1838a865b446db7c879d818bce461
Irina <hikarinakano95@gmail.com>
2024-11-18 13:39:25 +0100

    Added translations and label to Dropdown component

--- frontend/src/components/chat/Channel.js
+++ frontend/src/components/chat/Channel.js
@@ -1,37 +1,39 @@
 import { Button, Dropdown } from 'react-bootstrap';
+import { usePageTranslation } from '../../hooks/usePageTranslation';
 
 const Channel = ({ channel, currentChannel, onChannelSelect, onChannelDelete, onChannelEdit }) => {
   const isDefault = ['general', 'random'].includes(channel.name);
-    const channelName = `# ${channel.name}`;
-    const { id } = currentChannel;
-    return (
-      <li key={channel.id} className="nav-item d-flex mb-2">
-        <Button
-          variant={id === channel.id ? 'primary' : 'light'}
-          className="text-start w-100 text-truncate"
-          onClick={() => onChannelSelect(channel)}
-        >
-          {channelName}
-        </Button>
+  const translation = usePageTranslation('chat')
+  const channelName = `# ${channel.name}`;
+  const { id } = currentChannel;
+  return (
+    <li key={channel.id} className="nav-item d-flex mb-2">
+      <Button
+        variant={id === channel.id ? 'primary' : 'light'}
+        className="text-start w-100 text-truncate"
+        onClick={() => onChannelSelect(channel)}
+      >
+        {channelName}
+      </Button>
 
-        {!isDefault && (
-          <Dropdown align="end">
-            <Dropdown.Toggle variant="light" id={`channel-${channel.id}-dropdown`}>
-              <span className="visually-hidden">Channel menu</span>
-            </Dropdown.Toggle>
+      {!isDefault && (
+        <Dropdown align="end">
+          <Dropdown.Toggle variant="light" id={`channel-${channel.id}-dropdown`}>
+            <span className="visually-hidden">{translation('manageChannelLabel')}</span>
+          </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => onChannelDelete(channel.id)}>
-                Delete
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => onChannelEdit(channel.id)}>
-                Edit
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        )}
-      </li>
-    );
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => onChannelDelete(channel.id)}>
+              {translation('dropdownDelete')}
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => onChannelEdit(channel.id)}>
+              {translation('dropdownEdit')}
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
+    </li>
+  );
 }
 
 export default Channel;
