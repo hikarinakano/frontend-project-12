df4d01b02dd8401ddcbd7780841030cd99dc3675
Irina <hikarinakano95@gmail.com>
2024-11-21 13:52:33 +0100

    Linter fix

--- frontend/src/components/pages/NotFoundPage.js
+++ frontend/src/components/pages/NotFoundPage.js
@@ -9,15 +9,19 @@ const NotFoundPage = () => {
     <div className="h-100" id="chat">
       <div className="d-flex flex-column h-100">
         <div className="text-center bg-light">
-          <img 
-            alt={t('notFound.header')} 
-            className="img-fluid h-25" 
+          <img
+            alt={t('notFound.header')}
+            className="img-fluid h-25"
             src={notFoundImage}
           />
-          <h1 className="h4 text-muted">{t('notFound.header')}</h1>
+          <h1 className="h4 text-muted">
+            {t('notFound.header')}
+          </h1>
           <p className="text-muted">
-            {t('notFound.goto')}{' '}
-            <Link to="/">{t('notFound.mainPage')}</Link>
+            {t('notFound.goto')}
+            <Link to="/">
+              {t('notFound.mainPage')}
+            </Link>
           </p>
         </div>
         <div className="Toastify"></div>
