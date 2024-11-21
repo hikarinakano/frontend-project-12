0549363176cf07c4ab100f4ee0ff50891ab21d52
Irina <hikarinakano95@gmail.com>
2024-11-21 13:31:41 +0100

    Fixed the right color on the background of the page

--- frontend/src/components/pages/NotFoundPage.js
+++ frontend/src/components/pages/NotFoundPage.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
   return (
     <div className="h-100" id="chat">
       <div className="d-flex flex-column h-100">
-        <div className="text-center">
+        <div className="text-center bg-light">
           <img 
             alt={t('notFound.header')} 
             className="img-fluid h-25" 
