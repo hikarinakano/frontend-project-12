1853ec4a3ca63e3bdc716452793514f13045013f
Irina <hikarinakano95@gmail.com>
2024-12-03 12:44:56 +0100

    Added rules about the unused-vars

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,7 +4,8 @@ import pluginReact from "eslint-plugin-react";
 import pluginImport from "eslint-plugin-import";
 
 export default [
-  { 
+  pluginJs.configs.recommended,
+  {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
       globals: {
@@ -38,11 +39,23 @@ export default [
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
-      "import/extensions": "off"
+      "react/jsx-uses-vars": "error",
+      "import/extensions": "off",
+      "no-unused-vars": "off"
     }
   },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: {
+      react: pluginReact
+    },
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/jsx-uses-vars": "error"
+    }
+  },
   { ignores: ["/node_modules/", "dist", "build"] }
 ];
 
