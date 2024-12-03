715fdc54668025716300c91de69637ded10a0c32
Irina <hikarinakano95@gmail.com>
2024-12-03 12:12:12 +0100

    Added rules to fix issues on codeclimate with imports in some modules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,24 +1,41 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import pluginImport from "eslint-plugin-import";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: {
-    ...globals.browser,
-    ...globals.node,
-    process: 'readonly',
-  } } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        process: 'readonly',
+      },
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     plugins: {
-      react: pluginReact
+      react: pluginReact,
+      import: pluginImport
     },
     settings: {
       react: {
         version: "detect",
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.mjs']
+        }
       }
     }
   },
@@ -28,6 +45,11 @@ export default [
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
+      "import/extensions": ["error", "ignorePackages", {
+        "js": "never",
+        "jsx": "never",
+        "mjs": "never"
+      }]
     },
   }
 ];
