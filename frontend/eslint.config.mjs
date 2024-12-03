67b49eeda6edbe0f7f7340229119687cc29b1f73
Irina <hikarinakano95@gmail.com>
2024-12-03 12:25:04 +0100

    Added ignore for checking the import file extensions

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,8 +4,8 @@ import pluginReact from "eslint-plugin-react";
 import pluginImport from "eslint-plugin-import";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  {
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -13,17 +13,13 @@ export default [
         process: 'readonly',
       },
       sourceType: 'module',
-      ecmaVersion: 'latest',
+      ecmaVersion: 2022,
       parserOptions: {
         ecmaFeatures: {
           jsx: true
         }
       }
-    }
-  },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
+    },
     plugins: {
       react: pluginReact,
       import: pluginImport
@@ -37,20 +33,16 @@ export default [
           extensions: ['.js', '.jsx', '.mjs']
         }
       }
-    }
-  },
-  { ignores: ["/node_modules/", "dist", "build"] },
-  {
+    },
     rules: {
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
-      "import/extensions": ["error", "always", {
-        "js": "never",
-        "jsx": "never",
-        "mjs": "never"
-      }]
-    },
-  }
+      "import/extensions": "off"
+    }
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { ignores: ["/node_modules/", "dist", "build"] }
 ];
 
