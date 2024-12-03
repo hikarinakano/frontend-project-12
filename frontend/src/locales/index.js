9374840d389a678612200033ab3bd7e81c6d97ca
Irina <hikarinakano95@gmail.com>
2024-12-03 17:25:45 +0100

    Moved all the logic to init, here only export for the initialization

--- frontend/src/locales/index.js
+++ frontend/src/locales/index.js
@@ -1,31 +1,3 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import resources from './ru.js';
+import ru from './ru.js';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      ru: resources,
-    },
-    lng: 'ru',
-    fallbackLng: 'ru',
-    interpolation: {
-      escapeValue: false,
-    },
-    plurals: {
-      ru: {
-        numbers: [0, 1, 2, 5],
-        plurals: (n) => {
-          const lastTwo = n % 100;
-          const last = n % 10;
-          if (lastTwo >= 11 && lastTwo <= 19) return 'many';
-          if (last === 1) return 'one';
-          if (last >= 2 && last <= 4) return 'few';
-          return 'many';
-        },
-      },
-    },
-  });
-
-export default i18n;
+export { ru };
