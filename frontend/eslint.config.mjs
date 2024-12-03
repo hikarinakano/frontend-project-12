6e26aedabe0782390358f977862e0335a5c1f4b4
Irina <hikarinakano95@gmail.com>
2024-12-03 11:50:30 +0100

    Added rules according to the process.env to use in my rollbar config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,7 +5,11 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: {
+    ...globals.browser,
+    ...globals.node,
+    process: 'readonly',
+  } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
