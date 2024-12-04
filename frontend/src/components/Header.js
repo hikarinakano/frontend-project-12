4f7c41b1164bd0d62a27e9b197062a62261eec07
Irina <hikarinakano95@gmail.com>
2024-12-04 13:38:41 +0100

    Moved all the header connected components to a separate component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import { Navbar, Container } from "react-bootstrap";
+import LogOutButton from "./authorization/AuthButton";
+
+const Header = () => {
+  return (
+    <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <Container>
+        <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+        <LogOutButton />
+      </Container>
+    </Navbar>
+  )
+};
+
+export default Header;
