4dd3f1f2e5eb5ba8c83f8518a82434fec5d6671f
Irina <hikarinakano95@gmail.com>
2024-12-04 13:38:11 +0100

    Added wrapping divs to separate component

---
+++ frontend/src/components/AppWrapper.js
@@ -0,0 +1,12 @@
+const AppWrapper = ({ children }) => {
+  return (
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100 bg-light">
+          {children}
+        </div>
+      </div>
+    </div>)
+}
+
+export default AppWrapper;
