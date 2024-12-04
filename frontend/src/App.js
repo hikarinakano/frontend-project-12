4465ae5da93ee11339f7ab9ca47f6afcdfc553ef
Irina <hikarinakano95@gmail.com>
2024-12-04 14:44:04 +0100

    Removed the AuthProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import LoginPage from './components/pages/LoginPage.js';
 import ChatPage from './components/chat/ChatPage.js';
 import SignupPage from './components/pages/SignupPage.js';
 import PrivateRoute from './components/authorization/PrivateRoute.js';
-import AuthProvider from './components/authorization/AuthProvider.js';
 import NotFoundPage from './components/pages/NotFoundPage.js';
 import Header from './components/Header.js';
 import AppWrapper from './components/AppWrapper.js';
@@ -16,18 +15,16 @@ import AppWrapper from './components/AppWrapper.js';
 const App = () => (
   <AppWrapper>
     <Provider store={store}>
-      <AuthProvider>
-        <Header />
-        <Router>
-          <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
-        </Router>
-        <ToastContainer />
-      </AuthProvider>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </Provider>
   </AppWrapper>
 );
