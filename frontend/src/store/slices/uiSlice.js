018efb0d3a8731ae3461d65ed59d9b9aa02d8bc0
Irina <hikarinakano95@gmail.com>
2024-12-09 13:11:19 +0100

    Refactored the logic within state to separate channels and modals logic

--- frontend/src/store/slices/uiSlice.js
+++ frontend/src/store/slices/uiSlice.js
@@ -3,7 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 const DEFAULT_CHANNEL_ID = 1;
 
 const initialState = {
-  currentChannelId: null,
+  channels: {
+    currentChannelId: null,
+    defaultChannelId: DEFAULT_CHANNEL_ID,
+  },
   modal: {
     isOpen: false,
     type: null,
@@ -16,7 +19,10 @@ const uiSlice = createSlice({
   initialState,
   reducers: {
     setCurrentChannel: (state, { payload }) => {
-      state.currentChannelId = payload;
+      state.channels.currentChannelId = payload;
+    },
+    setDefaultChannel: (state) => {
+      state.channels.currentChannelId = state.channels.defaultChannelId;
     },
     openModal: (state, { payload: { type, extra = null } }) => {
       state.modal = { isOpen: true, type, extra };
@@ -27,11 +33,16 @@ const uiSlice = createSlice({
   },
 });
 
-export const { setCurrentChannel, openModal, closeModal } = uiSlice.actions;
+export const {
+  setCurrentChannel,
+  setDefaultChannel,
+  openModal,
+  closeModal,
+} = uiSlice.actions;
 
 export const selectors = {
-  selectCurrentChannelId: (state) => state.ui.currentChannelId,
-  selectDefaultChannelId: (state) => state.ui.defaultChannelId,
+  selectCurrentChannelId: (state) => state.ui.channels.currentChannelId,
+  selectDefaultChannelId: (state) => state.ui.channels.defaultChannelId,
   selectModal: (state) => state.ui.modal,
 };
 
