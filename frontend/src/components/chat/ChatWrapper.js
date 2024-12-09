91618a419eb8d2cfda37b3eda41b2b3e2aa21e4b
Irina <hikarinakano95@gmail.com>
2024-12-09 16:01:35 +0100

    Linter fix

--- frontend/src/components/chat/ChatWrapper.js
+++ frontend/src/components/chat/ChatWrapper.js
@@ -1,15 +1,13 @@
 const Wrapper = ({ channels, chat }) =>
-  (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-      <div className="row h-100 bg-white flex-md-row">
-        <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-          {channels}
-        </div>
-        <div className="col p-0 h-100">
-          {chat}
-        </div>
+  (<div className="container h-100 my-4 overflow-hidden rounded shadow">
+    <div className="row h-100 bg-white flex-md-row">
+      <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+        {channels}
+      </div>
+      <div className="col p-0 h-100">
+        {chat}
       </div>
     </div>
-  );
+  </div>);
 
 export default Wrapper;
