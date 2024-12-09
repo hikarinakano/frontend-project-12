b410582436593ef29d57113ba20a5ad8444ebc53
Irina <hikarinakano95@gmail.com>
2024-12-09 13:01:27 +0100

    Plugged in new slice to the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import { channelsApi } from './api/channelsApi';
 import { messagesApi } from './api/messagesApi';
+import uiReducer from './slices/uiSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
+    ui: uiReducer,
   },
   middleware:
     (getDefaultMiddleware) => getDefaultMiddleware()
