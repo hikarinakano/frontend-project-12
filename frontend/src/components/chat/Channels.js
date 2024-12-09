25e075c1fe4cd1aa78e337fa049c77807b5a4010
Irina <hikarinakano95@gmail.com>
2024-12-09 13:17:26 +0100

    Refactored code to use selectors from ui slice and pass them down to each Channel. Also imported svg accordingly to a new path in assets

--- frontend/src/components/chat/Channels.js
+++ frontend/src/components/chat/Channels.js
@@ -1,52 +1,23 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
 import { useGetChannelsQuery } from '../../store/api/channelsApi';
-import AddChannelModal from '../modals/AddChannelModal';
-import DeleteChannelModal from '../modals/DeleteChannelModal';
-import EditChannelModal from '../modals/EditChannelModal';
+import { setCurrentChannel, openModal, selectors } from '../../store/slices/uiSlice';
 import Channel from './Channel';
 import usePageTranslation from '../../hooks/usePageTranslation';
+import { ReactComponent as AddChannelSquare} from '../../assets/svg/add-channel.svg';
 
-const Channels = ({ currentChannel, onChannelSelect }) => {
-  const { username } = useSelector((state) => state.auth);
-  const { data: channels = [] } = useGetChannelsQuery(undefined, {
-    refetchOnMountOrArgChange: true,
-  });
-  const [showAddModal, setShowAddModal] = useState(false);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [channelToDelete, setChannelToDelete] = useState(null);
-  const [channelToEdit, setChannelToEdit] = useState(null);
-  const [showEditModal, setShowEditModal] = useState(false);
+const Channels = () => {
+  const dispatch = useDispatch();
+  const { data: channels = [] } = useGetChannelsQuery();
+  const currentChannelId = useSelector(selectors.selectCurrentChannelId);
   const t = usePageTranslation('chat');
 
-  const handleChannelDelete = (deletedChannelId) => {
-    if (deletedChannelId === currentChannel.id) {
-      const generalChannel = channels.find((c) => c.name === 'general');
-      if (generalChannel) {
-        onChannelSelect(generalChannel);
-      }
-    }
-  };
-  const handleShowDeleteModal = (channelId) => {
-    setChannelToDelete(channelId);
-    setShowDeleteModal(true);
-  };
-  const handleButtonClick = (e) => {
-    e.currentTarget.blur();
-    setShowAddModal(true);
+  const handleShowModal = (type, extra = null) => {
+    dispatch(openModal({ type, extra }));
   };
 
-  const handleAddChannel = (newChannel) => {
-    if (username === newChannel.username) {
-      onChannelSelect(newChannel);
-    }
-    setShowAddModal(false);
-  };
-
-  const handleChannelEdit = (channelId) => {
-    setChannelToEdit(channelId);
-    setShowEditModal(true);
+  const handleChannelSelect = (channelId) => {
+    dispatch(setCurrentChannel(channelId));
   };
 
   return (
@@ -56,12 +27,9 @@ const Channels = ({ currentChannel, onChannelSelect }) => {
         <Button
           variant="group-vertical"
           className="p-0 text-primary"
-          onClick={handleButtonClick}
+          onClick={() => handleShowModal('adding')}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-          </svg>
+          <AddChannelSquare />
           <span className="visually-hidden">+</span>
         </Button>
       </div>
@@ -70,31 +38,12 @@ const Channels = ({ currentChannel, onChannelSelect }) => {
           <Channel
             key={channel.id}
             channel={channel}
-            currentChannel={currentChannel}
-            onChannelSelect={onChannelSelect}
-            onChannelDelete={handleShowDeleteModal}
-            onChannelEdit={handleChannelEdit}
+            currentChannelId={currentChannelId}
+            onChannelSelect={handleChannelSelect}
+            onShowModal={handleShowModal}
           />
         ))}
       </ul>
-
-      <AddChannelModal
-        show={showAddModal}
-        onHide={() => setShowAddModal(false)}
-        onChannelAdd={handleAddChannel}
-      />
-      <DeleteChannelModal
-        show={showDeleteModal}
-        onHide={() => setShowDeleteModal(false)}
-        channelId={channelToDelete}
-        onChannelDelete={handleChannelDelete}
-      />
-      <EditChannelModal
-        show={showEditModal}
-        onHide={() => setShowEditModal(false)}
-        channelId={channelToEdit}
-        onChannelEdit={handleChannelEdit}
-      />
     </>
   );
 };
