91fe0a8981a410ffe9f33a7daa8854cb6ee3f415
Irina <hikarinakano95@gmail.com>
2024-12-09 11:59:09 +0100

    Moved the ChatPage to the pages folder

--- frontend/src/components/pages/ChatPage.js
+++ frontend/src/components/pages/ChatPage.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useGetChannelsQuery } from '../../store/api/channelsApi';
-import Channels from './Channels';
-import Chat from './Chat';
+import Channels from '../chat/Channels';
+import Chat from '../chat/Chat';
+import ChatSkeleton from '../chat/skeletons/ChatSkeleton';
+import ChannelsSkeleton from '../chat/skeletons/ChannelsSkeleton';
 
 const ChatPage = () => {
   const { data: channels, isLoading } = useGetChannelsQuery();
@@ -20,7 +22,20 @@ const ChatPage = () => {
     setCurrentChannel(channel);
   };
 
-  if (isLoading || !currentChannel) return <div>Loading...</div>;
+  if (isLoading || !currentChannel) {
+    return (
+      <div className="container h-100 my-4 overflow-hidden rounded shadow">
+        <div className="row h-100 bg-white flex-md-row">
+          <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+            <ChannelsSkeleton />
+          </div>
+          <div className="col p-0 h-100">
+            <ChatSkeleton />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
