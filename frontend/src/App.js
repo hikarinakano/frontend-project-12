19b20d109059dc3da9de10e334b5f10b3857ee34
Irina <hikarinakano95@gmail.com>
2024-12-10 11:17:11 +0100

    Moved pagesList to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,29 @@ import './assets/styles/main.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import getRoutes from './navigation/getRoutes.js';
-import pagesList from './navigation/pagesList.js';
+import { pageRoutes } from './routes.js';
 import PrivateRoute from './components/authorization/PrivateRoute.js';
 import Header from './components/Header.js';
 import AppWrapper from './components/AppWrapper.js';
+import LoginPage from './components/pages/LoginPage.js';
+import ChatPage from './components/pages/ChatPage.js';
+import SignupPage from './components/pages/SignupPage.js';
+import NotFoundPage from './components/pages/NotFoundPage.js';
+
+const pagesList = {
+  ChatPage: <ChatPage />,
+  LoginPage: <LoginPage />,
+  SignupPage: <SignupPage />,
+  NotFoundPage: <NotFoundPage />,
+};
+
 
 const App = () => (
   <AppWrapper>
     <Header />
     <Router>
       <Routes>
-        {getRoutes().map(({
+        {pageRoutes().map(({
           key,
           path,
           component,
