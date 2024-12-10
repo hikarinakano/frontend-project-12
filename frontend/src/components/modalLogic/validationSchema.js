0046d850f0ead9c9eb2505f77db35504aecf4e85
Irina <hikarinakano95@gmail.com>
2024-12-10 11:13:22 +0100

    Separated render logic and validation and created a modal logic folder

---
+++ frontend/src/components/modalLogic/validationSchema.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+const getValidationSchema = (t, channels) => yup.object().shape({
+  name: yup
+    .string()
+    .required(t('errors.required'))
+    .min(3, t('errors.length'))
+    .max(20, t('errors.length'))
+    .test(
+      'unique',
+      t('errors.unique'),
+      (value) => {
+        if (!value) return true;
+        const trimmedValue = value.trim().toLowerCase();
+        return !channels.some(
+          (channel) => channel.name.trim().toLowerCase() === trimmedValue,
+        );
+      },
+    ),
+});
+
+export default getValidationSchema;
