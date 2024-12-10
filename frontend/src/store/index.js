a28ab344ad71fd183377207806ffca56a014b78a
Irina <hikarinakano95@gmail.com>
2024-12-10 16:22:33 +0100

    Included new api to the store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,20 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import uiReducer from './slices/uiSlice';
 import { channelsApi } from './api/channelsApi';
 import { messagesApi } from './api/messagesApi';
-import uiReducer from './slices/uiSlice';
+import { authApi } from './api/authApi';
+
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [channelsApi.reducerPath]: channelsApi.reducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
     ui: uiReducer,
   },
   middleware:
     (getDefaultMiddleware) => getDefaultMiddleware()
       .concat(channelsApi.middleware)
-      .concat(messagesApi.middleware),
+      .concat(messagesApi.middleware)
+      .concat(authApi.middleware),
 });
 
 export default store;
