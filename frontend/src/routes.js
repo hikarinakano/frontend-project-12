32b3c3d67287acaaed0f1b6147bf4a3bcf26cd96
Irina <hikarinakano95@gmail.com>
2024-12-10 11:13:58 +0100

    Moved all the Pages routes to the routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,8 +1,34 @@
 const apiPath = '/api/v1';
 
-export default {
+const apiRoutes = {
   loginPath: () => [apiPath, 'login'].join('/'),
   signupPath: () => [apiPath, 'signup'].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
 };
+
+const pageRoutes = () => [
+  {
+    key: 'chat',
+    path: '/',
+    component: 'ChatPage',
+    private: true,
+  },
+  {
+    key: 'login',
+    path: '/login',
+    component: 'LoginPage',
+  },
+  {
+    key: 'signup',
+    path: '/signup',
+    component: 'SignupPage',
+  },
+  {
+    key: 'notFound',
+    path: '*',
+    component: 'NotFoundPage',
+  },
+];
+
+export { apiRoutes, pageRoutes };
