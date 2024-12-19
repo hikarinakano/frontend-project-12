b5f86358c495d1685446488e0e0991e570b6584e
Irina <hikarinakano95@gmail.com>
2024-12-19 14:42:14 +0100

    Linter fix

--- frontend/src/components/Chat/Channels/Channel.js
+++ frontend/src/components/Chat/Channels/Channel.js
@@ -1,6 +1,5 @@
-import { Button } from 'react-bootstrap';
-import { Dropdown, ButtonGroup } from 'react-bootstrap';
-import { useTranslation } from "react-i18next";
+import { Dropdown, ButtonGroup, Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 const Channel = ({ channel, isCurrent, onChannelSelect, isRemovable, onShowModal }) => {
   const { t } = useTranslation();
@@ -9,7 +8,7 @@ const Channel = ({ channel, isCurrent, onChannelSelect, isRemovable, onShowModal
       <Dropdown as={ButtonGroup} className="d-flex">
         <Button
           type="button"
-          variant={isCurrent ? 'secondary' : null}
+          variant={isCurrent ? "secondary" : null}
           className="w-100 rounded-0 text-start text-truncate"
           onClick={() => onChannelSelect(channel.id)}
         >
@@ -43,7 +42,7 @@ const Channel = ({ channel, isCurrent, onChannelSelect, isRemovable, onShowModal
         <span className="me-1">#</span>
         {channel.name}
       </Button>)
-  )
+  );
 };
 
 export default Channel;
