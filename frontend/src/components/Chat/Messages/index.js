d22fce1046dc035e2bf1fc20e5e919d31c7130cd
Irina <hikarinakano95@gmail.com>
2024-12-19 15:55:50 +0100

    Refactored code so the amount of props passed to children will be minimal

--- frontend/src/components/Chat/Messages/index.js
+++ frontend/src/components/Chat/Messages/index.js
@@ -1,23 +1,19 @@
-import { useSelector } from 'react-redux';
-import { uiSelectors } from '../../../store/slices/uiSlice';
 import Header from './Header';
 import MessageForm from './Form';
 import MessageList from './MessageList';
 
 const Messages = ({ currentChannel, messages = [] }) => {
-  const currentChannelId = useSelector(uiSelectors.selectCurrentChannelId); 
   const channelMessages = messages.filter(
-    (message) => message.channelId === currentChannelId,
+    (message) => message.channelId === currentChannel.id,
   );
-
   return (
     <div className="d-flex flex-column h-100">
       <Header
-        channelName={currentChannel?.name}
+        channelName={currentChannel.name}
         messagesCount={channelMessages.length}
       />
       <MessageList channelMessages={channelMessages} />
-      <MessageForm currentChannelId={currentChannelId} />
+      <MessageForm />
     </div>
   );
 };
