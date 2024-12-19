83907c4c210044564fa8a57468bd96a8ad9d93a9
Irina <hikarinakano95@gmail.com>
2024-12-19 14:43:16 +0100

    Linter fix

--- frontend/src/components/Chat/Channels/ChannelsList.js
+++ frontend/src/components/Chat/Channels/ChannelsList.js
@@ -1,6 +1,11 @@
 import Channel from './Channel';
 
-const ChannelList = ({ channels, currentChannelId, onShowModal, onChannelSelect }) => (
+const ChannelList = ({
+  channels,
+  currentChannelId,
+  onShowModal,
+  onChannelSelect
+}) => (
   <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
     {channels.map((channel) => (
       <li key={channel.id} className="nav-item w-100">
