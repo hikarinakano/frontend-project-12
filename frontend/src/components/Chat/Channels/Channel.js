7996e52c9af0f9c39aa94ff673334d4879b3ad40
Irina <hikarinakano95@gmail.com>
2024-12-19 14:13:50 +0100

    Merged the channels button logic simply into the Channel component

--- frontend/src/components/Chat/Channels/Channel.js
+++ frontend/src/components/Chat/Channels/Channel.js
@@ -1,50 +1,49 @@
+import { Button } from 'react-bootstrap';
 import { Dropdown, ButtonGroup } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
-import ChannelButton from './ChannelButton';
+import { useTranslation } from "react-i18next";
 
-const Channel = ({
-  channel,
-  currentChannelId,
-  onChannelSelect,
-  onShowModal,
-}) => {
+const Channel = ({ channel, isCurrent, onChannelSelect, isRemovable, onShowModal }) => {
   const { t } = useTranslation();
-  const isActive = channel.id === currentChannelId;
-  const isDefaultChannel = ['general', 'random'].includes(channel.name);
-
-  const channelButton = (
-    <ChannelButton
-      channel={channel}
-      isActive={isActive}
-      onChannelSelect={onChannelSelect}
-    />
-  );
-
   return (
-    <li className="nav-item w-100">
-      {isDefaultChannel ? channelButton : (
-        <Dropdown as={ButtonGroup} className="d-flex">
-          {channelButton}
-          <Dropdown.Toggle
-            split
-            type="button"
-            variant={isActive ? 'secondary' : null}
-            className="flex-grow-0"
-          >
-            <span className="visually-hidden">{t('chat.manageChannelLabel')}</span>
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={() => onShowModal('removing', channel.id)}>
-              {t('chat.dropdownDelete')}
-            </Dropdown.Item>
-            <Dropdown.Item onClick={() => onShowModal('renaming', channel.id)}>
-              {t('chat.dropdownEdit')}
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      )}
-    </li>
-  );
+    isRemovable ? (
+      <Dropdown as={ButtonGroup} className="d-flex">
+        <Button
+          type="button"
+          variant={isCurrent ? 'secondary' : null}
+          className="w-100 rounded-0 text-start text-truncate"
+          onClick={() => onChannelSelect(channel.id)}
+        >
+          <span className="me-1">#</span>
+          {channel.name}
+        </Button>
+        <Dropdown.Toggle
+          split
+          type="button"
+          variant={isCurrent ? 'secondary' : null}
+          className="flex-grow-0"
+        >
+          <span className="visually-hidden">{t('chat.manageChannelLabel')}</span>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => onShowModal('removing', channel.id)}>
+            {t('chat.dropdownDelete')}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => onShowModal('renaming', channel.id)}>
+            {t('chat.dropdownEdit')}
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    ) :
+      (<Button
+        type="button"
+        variant={isCurrent ? 'secondary' : null}
+        className="w-100 rounded-0 text-start text-truncate"
+        onClick={() => onChannelSelect(channel.id)}
+      >
+        <span className="me-1">#</span>
+        {channel.name}
+      </Button>)
+  )
 };
 
 export default Channel;
