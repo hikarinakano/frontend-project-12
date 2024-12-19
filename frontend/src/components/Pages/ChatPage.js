ce5cacf492c0f516f1227328dad653b85b8d2cbb
Irina <hikarinakano95@gmail.com>
2024-12-19 16:20:59 +0100

    Refactored the component to pass the queries and id to the components properly

--- frontend/src/components/Pages/ChatPage.js
+++ frontend/src/components/Pages/ChatPage.js
@@ -41,7 +41,7 @@ const ChatPage = () => {
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-          <Channels channels={channels} currentChannel={currentChannel}/>
+          <Channels channels={channels} currentChannelId={currentChannelId}/>
         </div>
         <div className="col p-0 h-100">
             <Messages
