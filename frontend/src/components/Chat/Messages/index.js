b12d0a2e3e8d154ec28b43d27bcef591e2281a28
Irina <hikarinakano95@gmail.com>
2024-12-19 14:21:11 +0100

    Renamed the component to Messages

--- frontend/src/components/Chat/Messages/index.js
+++ frontend/src/components/Chat/Messages/index.js
@@ -1,27 +1,25 @@
 import { useSelector } from 'react-redux';
-import { useGetMessagesQuery } from '../../../store/api/messagesApi';
-import { useGetChannelsQuery } from '../../../store/api/channelsApi';
 import { uiSelectors } from '../../../store/slices/uiSlice';
 import Header from './Header';
 import MessageForm from './Form';
 import MessageList from './MessageList';
 
-const Chat = () => {
+const Messages = ({ currentChannel, messages = [] }) => {
   const currentChannelId = useSelector(uiSelectors.selectCurrentChannelId);
-  const { data: channels = [] } = useGetChannelsQuery();
-  const { data: messages = [] } = useGetMessagesQuery();
-
-  const currentChannel = channels.find((channel) => channel.id === currentChannelId);
   const channelMessages = messages.filter(
     (message) => message.channelId === currentChannelId,
   );
+
   return (
     <div className="d-flex flex-column h-100">
-      <Header channelName={currentChannel.name} messagesCount={channelMessages.length} />
+      <Header 
+        channelName={currentChannel?.name} 
+        messagesCount={channelMessages.length} 
+      />
       <MessageList channelMessages={channelMessages} />
       <MessageForm currentChannelId={currentChannelId} />
     </div>
   );
 };
 
-export default Chat;
+export default Messages;
