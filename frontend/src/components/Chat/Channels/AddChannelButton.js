651199037f14272712b1214a2b55ec559413a924
Irina <hikarinakano95@gmail.com>
2024-12-19 13:38:31 +0100

    Separated the add channel button logic to its own module

---
+++ frontend/src/components/Chat/Channels/AddChannelButton.js
@@ -0,0 +1,21 @@
+import { Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import AddIcon from '../../../assets/pictures/add-icon.svg';
+
+const AddChannelButton = ({ handleShowModal }) => {
+  const { t } = useTranslation();
+
+  return (<div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+    <b>{t('chat.channels')}</b>
+    <Button
+      variant="group-vertical"
+      className="p-0 text-primary"
+      onClick={() => handleShowModal('adding')}
+    >
+      <img src={AddIcon} className="icon-blue" alt="add icon" />
+      <span className="visually-hidden">+</span>
+    </Button>
+  </div>);
+};
+
+export default AddChannelButton;
