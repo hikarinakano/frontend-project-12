b7ff1acc018b946ded80436bea91918f82222b2b
Irina <hikarinakano95@gmail.com>
2024-12-19 17:43:50 +0100

    Moved all the autButton logic to the header and applied it to simple react-bootstrap button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,28 @@
 import { Navbar, Container } from 'react-bootstrap';
-import LogOutButton from './authorization/AuthButton';
+import { Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectors } from '../store/slices/authSlice';
 
-const Header = () => (
-  <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-    <Container>
-      <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-      <LogOutButton />
-    </Container>
-  </Navbar>
-);
+const Header = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const loggedIn = useSelector(selectors.selectIsLoggedIn);
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  return (
+    <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+      <Container>
+        <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+        {loggedIn ? (
+          <Button variant="primary" onClick={handleLogout}>
+            {t('logOutBtn')}
+          </Button>
+        ) : null}
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
