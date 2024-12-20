30629584555a26fe74e2dd6c190667b0739d6c0e
Irina <hikarinakano95@gmail.com>
2024-12-20 10:25:46 +0100

    Refactored code for the page to render in accordance to children components

--- frontend/src/components/Pages/SignupPage.js
+++ frontend/src/components/Pages/SignupPage.js
@@ -1,12 +1,8 @@
 import { useEffect, useRef } from 'react';
-import { Card } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
-import SignupForm from '../Signup/SignupForm';
-import signupPic from '../../assets/pictures/sign-in-logo.jpg';
+import Signup from '../Signup';
 
 const SignupPage = () => {
   const inputRef = useRef();
-  const { t } = useTranslation();
 
   useEffect(() => {
     inputRef.current?.focus();
@@ -16,18 +12,7 @@ const SignupPage = () => {
     <div className="container-fluid h-100">
       <div className="row justify-content-center align-content-center h-100">
         <div className="col-12 col-md-8 col-xxl-6">
-          <Card className="shadow-sm">
-            <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-              <div>
-                <img
-                  src={signupPic}
-                  className="rounded-circle"
-                  alt={t('signup.signupHeader')}
-                />
-              </div>
-              <SignupForm inputRef={inputRef} />
-            </Card.Body>
-          </Card>
+          <Signup inputRef={inputRef} />
         </div>
       </div>
     </div>
