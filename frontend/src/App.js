84b8443cd94b41507de773909db120e40bab3288
Irina <hikarinakano95@gmail.com>
2024-12-20 13:21:36 +0100

    Provided the isAuth prop to the private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './assets/styles/main.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectors } from './store/slices/authSlice.js';
 import { PAGES } from './routes.js';
 import PrivateRoute from './components/authorization/PrivateRoute';
 import Header from './components/Header';
@@ -11,21 +13,26 @@ import ChatPage from './components/Pages/ChatPage';
 import SignupPage from './components/Pages/SignupPage';
 import NotFoundPage from './components/Pages/NotFoundPage';
 
-const App = () => (
-  <AppWrapper>
-    <Header />
-    <Router>
-      <Routes>
-        <Route path={PAGES.getLogin()} element={<LoginPage />} />
-        <Route path={PAGES.getSignup()} element={<SignupPage />} />
-        <Route element={<PrivateRoute />}>
-          <Route path={PAGES.getChat()} element={<ChatPage />} />
-        </Route>
-        <Route path={PAGES.getNotFound()} element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-    <ToastContainer />
-  </AppWrapper>
-);
+const App = () => {
+  const isAuth = useSelector(selectors.selectIsLoggedIn);
+  return (
+
+    <AppWrapper>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path={PAGES.getLogin()} element={<LoginPage />} />
+          <Route path={PAGES.getSignup()} element={<SignupPage />} />
+          <Route element={<PrivateRoute isAuth={isAuth} />}>
+            <Route path={PAGES.getChat()} element={<ChatPage />} />
+          </Route>
+          <Route path={PAGES.getNotFound()} element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </AppWrapper>
+  );
+};
+
 
 export default App;
--- frontend/src/components/authorization/PrivateRoute.js
+++ frontend/src/components/authorization/PrivateRoute.js
@@ -1,10 +1,7 @@
 import { Outlet, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectors } from '../../store/slices/authSlice';
 import { PAGES } from '../../routes';
 
-const PrivateRoute = () => {
-  const isAuth = useSelector(selectors.selectIsLoggedIn);
+const PrivateRoute = ({ isAuth }) => {
   return (
     isAuth ? <Outlet /> : <Navigate to={PAGES.getLogin()} />
   );
