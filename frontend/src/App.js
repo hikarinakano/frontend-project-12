1d4637fd8fb6b0962fabd89a4a650dd6e66dd5f1
Irina <hikarinakano95@gmail.com>
2024-12-21 10:56:26 +0100

    Wrapped everything in App in Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import './assets/styles/main.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectors } from './store/slices/authSlice.js';
 import { PAGES } from './routes.js';
 import PrivateRoute from './components/authorization/PrivateRoute';
 import Header from './components/Header';
@@ -14,23 +12,21 @@ import SignupPage from './components/Pages/SignupPage';
 import NotFoundPage from './components/Pages/NotFoundPage';
 
 const App = () => {
-  const isAuth = useSelector(selectors.selectIsLoggedIn);
   return (
-
-    <AppWrapper>
-      <Header />
-      <Router>
+    <Router>
+      <AppWrapper>
+        <Header />
         <Routes>
           <Route path={PAGES.getLogin()} element={<LoginPage />} />
           <Route path={PAGES.getSignup()} element={<SignupPage />} />
-          <Route element={<PrivateRoute isAuth={isAuth} />}>
+          <Route element={<PrivateRoute />}>
             <Route path={PAGES.getChat()} element={<ChatPage />} />
           </Route>
           <Route path={PAGES.getNotFound()} element={<NotFoundPage />} />
         </Routes>
-      </Router>
-      <ToastContainer />
-    </AppWrapper>
+        <ToastContainer />
+      </AppWrapper>
+    </Router>
   );
 };
 
