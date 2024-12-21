fcf1d2612037449b29762397b9a51b21d5f1cdc3
Irina <hikarinakano95@gmail.com>
2024-12-21 11:01:01 +0100

    Handle the errors directly as i could not use the uiSlice properly to handle error messages

--- frontend/src/components/Login/LoginForm.js
+++ frontend/src/components/Login/LoginForm.js
@@ -1,11 +1,10 @@
 import { useFormik } from 'formik';
 import { Button, Form } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useLoginMutation } from '../../store/api/authApi';
-import { setAuthError, cleanError, uiSelectors } from '../../store/slices/uiSlice';
 import { login } from '../../store/slices/authSlice';
 import { PAGES } from '../../routes';
 import FormField from './FormField';
@@ -15,7 +14,6 @@ const LoginForm = ({ inputRef }) => {
   const [loginUser] = useLoginMutation();
   const { t } = useTranslation();
   const location = useLocation();
-  const isAuthFailed = useSelector(uiSelectors.selectIsAuthError);
   const navigate = useNavigate();
 
   const formik = useFormik({
@@ -26,23 +24,22 @@ const LoginForm = ({ inputRef }) => {
     onSubmit: async (values, { setErrors }) => {
       try {
         setErrors({});
-        dispatch(cleanError());
         const userData = await loginUser(values).unwrap();
         dispatch(login(userData));
         const { from } = location.state || { from: { pathname: PAGES.getChat() } };
         navigate(from);
       } catch (err) {
-        if (!err.status) {
+        if (err.status === 'NETWORK_ERROR' || err.status === 'FETCH_ERROR') {
           toast.error(t('notifications.connection'));
-        } else {
-          dispatch(setAuthError({ 
-            type: 'AuthError', 
-            code: err.status === 401 ? 'unauthorized' : 'unknown' 
-          }));
+        } else if (err.status === 401) {
+          setErrors({
+            username: t('errors.login'),
+            password: t('errors.login'),
+          });
         }
         inputRef.current.select();
       }
-    },
+    }
   });
 
   return (
@@ -53,8 +50,8 @@ const LoginForm = ({ inputRef }) => {
         label={t('login.username')}
         value={formik.values.username}
         onChange={formik.handleChange}
-        isInvalid={isAuthFailed}
-        error={t('errors.login')}
+        isInvalid={formik.errors.username}
+        error={formik.errors.username}
         inputRef={inputRef}
         autoComplete="username"
       />
@@ -64,8 +61,8 @@ const LoginForm = ({ inputRef }) => {
         label={t('login.password')}
         value={formik.values.password}
         onChange={formik.handleChange}
-        isInvalid={isAuthFailed}
-        error={t('errors.login')}
+        isInvalid={formik.errors.password}
+        error={formik.errors.password}
         autoComplete="current-password"
       />
       <Button
