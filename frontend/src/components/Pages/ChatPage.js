1301abab8b359a6733148b1111ee0f5b9c88ac94
Irina <hikarinakano95@gmail.com>
2024-12-23 11:11:06 +0100

    Set the current channel using the default channel id

--- frontend/src/components/Pages/ChatPage.js
+++ frontend/src/components/Pages/ChatPage.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useGetChannelsQuery } from '../../store/api/channelsApi';
 import { useGetMessagesQuery } from '../../store/api/messagesApi';
-import { setDefaultChannel, uiSelectors } from '../../store/slices/uiSlice';
+import { setCurrentChannel, uiSelectors } from '../../store/slices/uiSlice';
 import Channels from '../Chat/Channels/index';
 import Messages from '../Chat/Messages/index';
 import ChatSkeleton from '../Chat/Skeletons/ChatSkeleton';
@@ -10,20 +10,18 @@ import ChannelsSkeleton from '../Chat/Skeletons/ChannelsSkeleton';
 
 const ChatPage = () => {
   const dispatch = useDispatch();
-  const { data: channels, isLoading: isChannelsLoading } = useGetChannelsQuery();
-  const { data: messages, isLoading: isMessagesLoading } = useGetMessagesQuery();
+  const { data: channels = [], isLoading: isChannelsLoading } = useGetChannelsQuery();
+  const { data: messages = [], isLoading: isMessagesLoading } = useGetMessagesQuery();
   const currentChannelId = useSelector(uiSelectors.selectCurrentChannelId);
-  const currentChannel = channels?.find((channel) => channel.id === currentChannelId);
+  const defaultChannelId = useSelector(uiSelectors.selectDefaultChannelId);
   const isLoading = isChannelsLoading || isMessagesLoading;
+  const currentChannel = channels.find((channel) => channel.id === currentChannelId);
   useEffect(() => {
     if (channels?.length > 0 && !currentChannelId) {
-      dispatch(setDefaultChannel());
+      dispatch(setCurrentChannel(defaultChannelId));
     }
-  }, [channels, currentChannelId, dispatch]);
+  }, [channels, currentChannelId, defaultChannelId, dispatch]);
 
-  console.log('is loading', isLoading);
-  console.log('current channel',currentChannel);
-  console.log('channels', channels)
   if (isLoading) {
     return (
       <div className="container h-100 my-4 overflow-hidden rounded shadow">
@@ -38,6 +36,7 @@ const ChatPage = () => {
       </div>
     );
   }
+
   return (
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
