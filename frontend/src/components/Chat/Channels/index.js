5b2598cc2a87b4035967da86e75c9e52b7dbb7c7
Irina <hikarinakano95@gmail.com>
2024-12-23 10:14:19 +0100

    Moved the channelList render logic to the index.js to avoid the naming confusion and unnecessary module

--- frontend/src/components/Chat/Channels/index.js
+++ frontend/src/components/Chat/Channels/index.js
@@ -1,5 +1,21 @@
-import ChannelList from './ChannelList';
+import Channel from './Channel';
 import AddChannelButton from './AddChannelButton';
+
+const ChannelList = ({
+  channels,
+  currentChannelId,
+}) => (
+  <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
+    {channels.map((channel) => (
+      <li key={channel.id} className="nav-item w-100">
+        <Channel
+          channel={channel}
+          isCurrent={channel.id === currentChannelId}
+        />
+      </li>
+    ))}
+  </ul>
+);
 
 const Channels = ({ channels, currentChannelId }) => (
   <>
