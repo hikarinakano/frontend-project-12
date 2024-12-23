c9d50dbc8afec7a06d38e3c5a705de65601fb5eb
Irina <hikarinakano95@gmail.com>
2024-12-23 15:38:05 +0100

    Added a space between words

--- frontend/src/components/Login/Footer.js
+++ frontend/src/components/Login/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     <Card.Footer className="p-4">
       <div className="text-center">
         <span>
-          {t('login.noAccount')}
+          {`${('login.noAccount')} `}
         </span>
         <Link to={PAGES.getSignup()}>
           {t('login.signup')}
