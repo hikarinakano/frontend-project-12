85101d0895befab8081fa369bd0633bc5d9c4811
Irina <hikarinakano95@gmail.com>
2024-12-23 14:09:01 +0100

    Imported all the separated modals and simplified the render logic of the component

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,96 +1,34 @@
 import { Modal } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
-import filter from 'leo-profanity';
-import { useRef, useEffect } from 'react';
-import {
-  closeModal,
-  uiSelectors,
-  setCurrentChannel,
-} from '../store/slices/uiSlice';
-import {
-  useAddChannelMutation,
-  useEditChannelMutation,
-  useDeleteChannelMutation,
-  useGetChannelsQuery,
-} from '../store/api/channelsApi';
-import renderModalContent from '../modalLogic/renderModalContent';
-import getValidationSchema from '../modalLogic/validationSchema';
+import { closeModal, uiSelectors } from '../store/slices/uiSlice';
+import AddModal from '../Modal/AddModal';
+import RenameModal from '../Modal/RenameModal';
+import DeleteModal from '../Modal/DeleteModal';
+
+const modalComponents = {
+  adding: AddModal,
+  renaming: RenameModal,
+  removing: DeleteModal,
+};
 
 const ModalComponent = () => {
   const dispatch = useDispatch();
-  const { type, isOpen, extra: channelId } = useSelector(uiSelectors.selectModal);
-  const { username } = useSelector((state) => state.auth);
-  const { data: channels = [] } = useGetChannelsQuery();
-  const [addChannel] = useAddChannelMutation();
-  const [editChannel] = useEditChannelMutation();
-  const [deleteChannel] = useDeleteChannelMutation();
+  const { type, isOpen, extra } = useSelector(uiSelectors.selectModal);
   const { t } = useTranslation();
-  const inputRef = useRef(null);
-
-  const currentChannel = channels.find((channel) => channel.id === channelId);
 
   const handleClose = () => {
     dispatch(closeModal());
   };
-
-  useEffect(() => {
-    if (inputRef.current) {
-      if (type === 'adding') {
-        inputRef.current.focus();
-      } else if (type === 'renaming') {
-        inputRef.current.select();
-      }
-    }
-  }, [type]);
-
-  const formik = useFormik({
-    initialValues: {
-      name: type === 'renaming' ? currentChannel?.name : '',
-    },
-    validationSchema: type !== 'removing' ? getValidationSchema(t, channels) : null,
-    validateOnChange: false,
-    enableReinitialize: true,
-    onSubmit: async (values, { resetForm }) => {
-      try {
-        if (type === 'adding') {
-          const trimmedName = values.name.trim();
-          const cleanedName = filter.clean(trimmedName);
-          const result = await addChannel({ name: cleanedName, username }).unwrap();
-          dispatch(setCurrentChannel(result.id));
-          resetForm();
-        } if (type === 'renaming') {
-          const trimmedName = values.name.trim();
-          const cleanedName = filter.clean(trimmedName);
-          await editChannel({ id: channelId, name: cleanedName }).unwrap();
-        } if (type === 'removing') {
-          await deleteChannel(channelId).unwrap();
-        }
-        dispatch(closeModal());
-        const notifications = {
-          adding: 'channelCreated',
-          renaming: 'channelRenamed',
-          removing: 'channelDeleted',
-        };
-        toast.success(t(`notifications.${notifications[type]}`));
-      } catch (err) {
-        toast.error(t('notifications.connection'));
-        dispatch(closeModal());
-      }
-    },
-  });
-
+  if (!type) return null;
+  const SpecificModal = modalComponents[type];
   return (
     <Modal show={isOpen} onHide={handleClose}>
-      {renderModalContent({
-        type,
-        handleClose,
-        formik,
-        inputRef,
-        t,
-      })}
+      <SpecificModal
+        onClose={handleClose}
+        channelId={extra}
+        t={t}
+      />
     </Modal>
   );
 };
