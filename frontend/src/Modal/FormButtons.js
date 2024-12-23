e925791baa8599a3472946d02ef75baaafedf4e8
Irina <hikarinakano95@gmail.com>
2024-12-23 15:06:34 +0100

    Refactored code to make the buttons for Modals more reusable

---
+++ frontend/src/Modal/FormButtons.js
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap"
+
+const FormButtons = ({ onClose, cancelText, submitText, handleAction, secondBtn}) => (
+  <div className="d-flex justify-content-end gap-2 mt-3">
+  <Button variant="secondary" onClick={onClose}>
+    {cancelText}
+  </Button>
+  <Button variant={secondBtn || 'primary'} type="submit" onClick={handleAction}>
+    {submitText}
+  </Button>
+</div>
+);
+
+export default FormButtons;
