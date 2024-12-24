0708ea1a0af6fc88346d0d63f09dc9f4592caa54
Irina <hikarinakano95@gmail.com>
2024-12-24 08:20:45 +0100

    Moved the component to a separate folder

---
+++ frontend/src/Modal/RemoveChannelModal/index.js
@@ -0,0 +1,40 @@
+import { toast } from 'react-toastify';
+import { useDeleteChannelMutation } from '../../store/api/channelsApi';
+import BaseModal from '../BaseModal';
+import FormButtons from '../FormButtons';
+import { useState } from 'react';
+
+const RemoveChannelModal = ({ onClose, channelId, t }) => {
+  const [deleteChannel] = useDeleteChannelMutation();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleDelete = async () => {
+    setIsSubmitting(true);
+    try {
+      await deleteChannel(channelId).unwrap();
+      onClose();
+      toast.success(t('notifications.channelDeleted'));
+    } catch (err) {
+      toast.error(t('errors.networkError'));
+      onClose();
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <BaseModal title={t('modals.delete.title')} onClose={onClose}>
+      <p className="lead">{t('modals.delete.confirm')}</p>
+      <FormButtons
+        onClose={onClose}
+        cancelText={t('modals.delete.cancel')}
+        submitText={t('modals.delete.submit')}
+        handleAction={handleDelete}
+        secondBtn="danger"
+        disabled={isSubmitting}
+      />
+    </BaseModal>
+  );
+};
+
+export default RemoveChannelModal;
